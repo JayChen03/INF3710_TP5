7877c1a5c9485726df302eb67fdc6b5aca37dd65
Jason Chen <jason.chen@polymtl.ca>
2024-04-06 18:01:53 -0400

    deleted unused tutorial files

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CommunicationService } from "./communication.service";
-import { GuestComponent } from "./guest/guest.component";
-import { HotelComponent } from "./hotel/hotel.component";
-import { RoomComponent } from "./room/room.component";
 import { OiseauComponent } from './oiseau/oiseau.component';
 import { InformationModalComponent } from './information-modal/information-modal.component';
 import { MatDialogModule } from "@angular/material/dialog";
@@ -18,9 +15,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    RoomComponent,
-    HotelComponent,
-    GuestComponent,
     OiseauComponent,
     InformationModalComponent,
   ],
--- client/src/app/communication.service.ts
+++ client/src/app/communication.service.ts
@@ -3,10 +3,6 @@ import { Injectable } from "@angular/core";
 // tslint:disable-next-line:ordered-imports
 import { of, Observable, Subject } from "rxjs";
 import { catchError } from "rxjs/operators";
-import { Hotel } from "../../../common/tables/Hotel";
-import { Room } from "../../../common/tables/Room";
-import { HotelPK } from "../../../common/tables/HotelPK";
-import { Guest } from "../../../common/tables/Guest";
 import { Oiseau } from "../../../common/tables/Oiseau";
 import { InformationModalComponent } from "./information-modal/information-modal.component";
 import { MatDialog } from "@angular/material/dialog";
@@ -51,66 +47,6 @@ export class CommunicationService {
     return this.http
       .put<number>(this.BASE_URL + "/birds/update", oiseau)
       .pipe(catchError(this.handleError<number>("updateBird")));
-  }
-
-  public getHotels(): Observable<Hotel[]> {
-    return this.http
-      .get<Hotel[]>(this.BASE_URL + "/hotels")
-      .pipe(catchError(this.handleError<Hotel[]>("getHotels")));
-  }
-
-  public insertHotel(hotel: Hotel): Observable<number> {
-    return this.http
-      .post<number>(this.BASE_URL + "/hotels/insert", hotel)
-      .pipe(catchError(this.handleError<number>("insertHotel")));
-  }
-
-  public updateHotel(hotel: Hotel): Observable<number> {
-    return this.http
-      .put<number>(this.BASE_URL + "/hotels/update", hotel)
-      .pipe(catchError(this.handleError<number>("updateHotel")));
-  }
-
-  public deleteHotel(hotelNb: string): Observable<number> {
-    return this.http
-      .post<number>(this.BASE_URL + "/hotels/delete/" + hotelNb, {})
-      .pipe(catchError(this.handleError<number>("deleteHotel")));
-  }
-
-  public getHotelPKs(): Observable<HotelPK[]> {
-    return this.http
-      .get<HotelPK[]>(this.BASE_URL + "/hotels/hotelNb")
-      .pipe(catchError(this.handleError<HotelPK[]>("getHotelPKs")));
-  }
-
-  public getRooms(hotelNb: string): Observable<Room[]> {
-    return this.http
-      .get<Room[]>(this.BASE_URL + `/rooms?hotelNb=${hotelNb}`)
-      .pipe(catchError(this.handleError<Room[]>("getRooms")));
-  }
-
-  public insertRoom(room: Room): Observable<number> {
-    return this.http
-      .post<number>(this.BASE_URL + "/rooms/insert", room)
-      .pipe(catchError(this.handleError<number>("inserHotel")));
-  }
-
-  public updateRoom(room: Room): Observable<number> {
-    return this.http
-      .put<number>(this.BASE_URL + "/rooms/update", room)
-      .pipe(catchError(this.handleError<number>("updateRoom")));
-  }
-
-  public deleteRoom(hotelNb: string, roomNb: string): Observable<number> {
-    return this.http
-      .post<number>(this.BASE_URL + `/rooms/delete/${hotelNb}/${roomNb}`, {})
-      .pipe(catchError(this.handleError<number>("deleteRoom")));
-  }
-
-  public getGuests(hotelNb: string, roomNb: string): Observable<Guest[]> {
-    return this.http
-      .get<Guest[]>(this.BASE_URL + `/guests/${hotelNb}/${roomNb}`)
-      .pipe(catchError(this.handleError<Guest[]>("getGuests")));
   }
 
   private handleError<T>(
